3d157a4ba3760563f7225910f84c4b2e5f492ce5
noireko <agustinfernandezrolon@gmail.com>
2026-02-07 19:08:51 -0300

    Added gameboard logic

---
+++ js/js.js
@@ -0,0 +1,18 @@
+function gameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(0);
+        }
+    }
+
+    return board
+}
+
+
+const board = gameBoard();
+console.table(board);
