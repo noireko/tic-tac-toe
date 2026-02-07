159778122996dfd7d81c77fd1e04dba521fce192
noireko <agustinfernandezrolon@gmail.com>
2026-02-07 19:41:46 -0300

    Added switchplayer logic

--- js/js.js
+++ js/js.js
@@ -1,3 +1,13 @@
+const cell1 = document.getElementById("1");
+const cell2 = document.getElementById("2");
+const cell3 = document.getElementById("3");
+const cell4 = document.getElementById("4");
+const cell5 = document.getElementById("5");
+const cell6 = document.getElementById("6");
+const cell7 = document.getElementById("7");
+const cell8 = document.getElementById("8");
+const cell9 = document.getElementById("9");
+
 function gameBoard() {
     const rows = 3;
     const columns = 3;
@@ -9,7 +19,6 @@ function gameBoard() {
             board[i].push(0);
         }
     }
-
     return board
 }
 
