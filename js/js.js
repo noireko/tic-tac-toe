4c35b1000cecaeebf846da53f4656f523b51a424
noireko <agustinfernandezrolon@gmail.com>
2026-02-07 23:46:53 -0300

    Added checkWinner function

--- js/js.js
+++ js/js.js
@@ -2,6 +2,14 @@ const cell = document.querySelectorAll(".cell")
 
 let currentPlayer = "X";
 
+function createUser(name) {
+    const userName = name;
+    let score = 0;
+    const getScore = () => score;
+    const giveScore = () => { score++; };
+    return { userName, getScore, giveScore };
+}
+
 function gameBoard() {
     const rows = 3;
     const columns = 3;
@@ -15,6 +23,8 @@ function gameBoard() {
     }
     return board
 }
+
+const board = gameBoard();
 
 function switchTurn() {
     if (currentPlayer === "X") {
@@ -35,21 +45,23 @@ cell.forEach(cell => {
     })
 });
 
-const winningChances = [
-    [board[0][0], board[0][1], board[0][2]], 
-    [board[1][0], board[1][1], board[1][2]], 
-    [board[2][0], board[2][1], board[2][2]],
-    [board[0][0], board[1][0], board[2][0]],
-    [board[0][2], board[1][2], board[2][2]], 
-    [board[2][0], board[2][1], board[2][2]],
-    [board[0][0], board[1][1], board[2][2]],
-    [board[0][2], board[1][1], board[2][0]],
-]
 
 
+function checkWinner() {
+    const winningChances = [
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][2], board[1][2], board[2][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]],
+    ];
 
-if (winningChances[0] === "X") {
-    alert("Ganó Player1")
-    cell.innerText = "";
-};
-
+    return winningChances.some(
+        combo => combo[0] !== "" &&
+            combo[0] === combo[1] &&
+            combo[1] === combo[2]
+    );
+}
